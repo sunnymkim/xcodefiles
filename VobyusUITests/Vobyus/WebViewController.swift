21a876ac0606da0d1122feea1ac12a7b097f6304
Sunny Kim <sunnykim@Sunnys-MacBook-Pro-2.local>
2021-04-13 22:00:55 -0700

    Initial Commit

---
+++ VobyusUITests/Vobyus/WebViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+import WebKit
+class WebViewController: UIViewController, WKNavigationDelegate {
+
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let url = URL(string: "https://www.voby.us")!
+        webView.load(URLRequest(url: url))
+    }
+}
